81c8091b49a64d8e033b19e4710e73f28db920f1
Adrian Ottiker <adrian@ottiker.com>
2016-10-14 18:31:33 +0200

    es6 flow client

--- client.js
+++ client.js
@@ -1,45 +1,38 @@
-var Flow = require('flow');
-var adapterSet;
+import Flow from 'flow';
 
-// load module instance composition (MIC)
-function mic (name, callback) {
+export default (event, options) => {
+    let flow = Flow({
+        cache: {},
+        read: (name, callback) => {
 
-    fetch('/_i/' + name + '.json').then(function(response) {
+            // TODO triple stream
+            fetch('/_i/' + name + '.json').then(function(response) {
 
-        if (!response.ok) {
-            return callback(new Error(response.statusText));
+                if (!response.ok) {
+                    return callback(new Error(response.statusText));
+                }
+
+                return response.json();
+
+            }).then(function (composition) {
+                callback(null, composition);
+            });
+        },
+        mod: (name, callback) => {
+            var node = document.createElement('script');
+            var path = name + '.js';
+            node.onload = function () {
+                node.remove();
+                callback(null, require(name));
+            };
+
+            // set url and append dom script elm to the document head
+            node.src = '/_m/' + path;
+            document.head.appendChild(node);
         }
-
-        return response.json();
-
-    }).then(function (composition) {
-        callback(null, composition);
-    });
+    })(event, options);
+    flow.on('error', error => console.error(error));
+    flow.on('data', error => console.data(error));
+    flow.end(1);
+    return flow;
 }
-
-// load module
-function mod (name, callback) {
-
-    var node = document.createElement('script');
-    var path = name + '.js';
-    node.onload = function () {
-        node.remove();
-        callback(null, require(name));
-    };
-
-    // set url and append dom script elm to the document head
-    node.src = '/_m/' + path;
-    document.head.appendChild(node);
-};
-
-module.exports = function (event, options) {
-
-    if (!adapterSet) {
-        options = options || {};
-        options.mod = mod;
-        options.mic = mic;
-        adapterSet = true;
-    }
-
-    return Flow(event, options);
-};
