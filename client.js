514894f2e0231c2b0345abd80b8c7c7cfa4c3dfe
Adrian Ottiker <adrian@ottiker.com>
2016-10-19 21:57:00 +0200

    add "client.js"

---
+++ client.js
@@ -0,0 +1,36 @@
+import Flow from 'flow';
+
+export default function flow (event, options) {
+    let flow = Flow({
+        cache: {},
+        read: (name, callback) => {
+
+            // TODO triple stream
+            fetch('/_i/' + name + '.json').then(response => {
+
+                if (!response.ok) {
+                    return callback(new Error(response.statusText));
+                }
+
+                return response.json();
+
+            }).then(composition => callback(null, composition));
+        },
+        mod: (name, callback) => {
+            var node = document.createElement('script');
+            var path = name + '.js';
+            node.onload = () => {
+                node.remove();
+                callback(null, require(name));
+            };
+
+            // set url and append dom script elm to the document head
+            node.src = '/_m/' + path;
+            document.head.appendChild(node);
+        }
+    })(event, options);
+    //flow.on('error', error => console.error(error));
+    //flow.on('data', error => console.log(error));
+    flow.end(1);
+    return flow;
+}
