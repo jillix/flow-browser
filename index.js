9de1347e828dc7a01e5e270276c0f47040cf0c88
Adrian Ottiker <adrian@ottiker.com>
2016-10-20 22:51:36 +0200

    add placeholder replacers

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 
 const bundler = require('./lib/browserify');
 const module_name = 'flow-browser';
+const replace_read = /FLOW_READ_URL/;
+const replace_module = /FLOW_MODULE_URL/;
 
 exports.client = function (args, data, next) {
 
-    // TODO merge entrypoint config with client adapter
     bundler(args.target, {
         file: module_name,
-        exposemodule_nameme
+        expose: module_name,
+        replace: [
+            {from: replace_read, to: process.flow_env.browser.read},
+            {from: replace_module, to: process.flow_env.browser.mod}
+        ]
     }, (err, module) => {
         data.file = module;
         next(err, data);
