36c7013f05cf32dd3bf35fb48eb36f296aada6d8
Adrian Ottiker <adrian@ottiker.com>
2016-10-23 18:22:02 +0200

    try to install the module before bundling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const fs = require('fs');
 const bundler = require('./lib/browserify');
 const module_name = 'flow-browser';
 const replace_read = /FLOW_READ_URL/;
@@ -20,12 +21,37 @@ exports.client = function (args, data, next) {
 };
 
 exports.bundle = function (args, data, next) {
-    let module_name = data.module.slice(0, -3);
-    bundler(args.target + '/' + module_name + '.js', {
-        file: module_name,
-        expose: module_name
-    }, (err, module) => {
-        data.file = args.target + '/' + module_name + '.js';
-        next(err, data);
+
+    const module_name = data.module.slice(0, -3);
+    const file_path = args.target + '/' + module_name + '.js';
+    const repo = data.owner + '/' + module_name + '#flow_v0.1.0';
+
+    fs.access(process.cwd() + '/node_modules/' + module_name, (err) => {
+console.log('Bundle:', err);
+        if (err) {
+             exec('npm i --prefix ' + process.cwd() + ' ' + repo, err => {
+
+                if (err) {
+                    return next(err);
+                }
+
+                bundler(file_path, {
+                    file: module_name,
+                    expose: module_name
+                }, (err, module) => {
+                    data.file = file_path;
+                    next(err, data);
+                });
+            });
+            return;
+        }
+
+        bundler(file_path, {
+            file: module_name,
+            expose: module_name
+        }, (err, module) => {
+            data.file = file_path;
+            next(err, data);
+        });
     });
 }; 
