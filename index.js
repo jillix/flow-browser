7b01563c9051d978e663dde694e6176f66f50507
Adrian Ottiker <adrian@ottiker.com>
2017-06-15 20:40:59 +0200

    update to flow changes

--- index.js
+++ index.js
@@ -1,60 +1,47 @@
-"use strict"
-
-const Flow = require("flow");
 const cache = {};
+Flow({
+    get: (id) => {
+        return cache[id];
+    },
+    set: (id, data) => {
+        return cache[id] = data;
+    },
+    del: (id) => {
+        delete cache[id];
+    },
+    seq: (sequenceId, role) => {
+        // TODO here we could load a sequence from the safenetwork
+        return fetch("/.sq/" + sequenceId).then((response) => {
 
-module.exports = (sequenceId, env) => {
-
-    if (!sequenceId) {
-        return console.error("Start sequence missing. Example: flow sequenceId");
-    }
-
-    const event = Flow({
-        cache: {
-            get: (id) => {
-                return cache[id];
-            },
-            set: (id, data) => {
-                return cache[id] = data;
-            },
-            del: (id) => {
-                delete cache[id];
+            if (!response.ok) {
+                return Promise.reject(response.statusText);
             }
-        },
-        seq: (sequenceId, role) => {
-            // TODO here we could load a sequence from the safenetwork
-            return fetch(env.sequence + sequenceId).then(response => {
 
-                if (!response.ok) {
-                    return Promise.reject(response.statusText);
-                }
-
-                return response.json();
-            });
-        },
-        fn: (fn_iri, role) => {
-            return new Promise((resolve, reject) => {
-                const node = document.createElement("script");
-                node.onload = () => {
-                    cache[fn_iri] = 1;
-                    node.remove();
-                    fn_iri = require(fn_iri);
-                    if (typeof fn_iri !== "function") {
-                        delete cache[fn_iri];
-                        return reject(new Error("Flow-browser.fn: \"" + exports + "\" in module \"" + module + "\" is not a function."));
-                    }
-                    resolve(fn_iri);
-                };
-
-                node.src = env.fn + fn_iri;
-                document.head.appendChild(node);
-            });
-        }
-    })({
-        sequence: sequenceId
-    });
-
-    event.on("error", error => {
-        console.error(error);
-    });
-};
+            return response.json();
+        });
+    },
+    fnc: (fn_iri, role) => {
+        // TODO here we could load the handler bundle form the safenetwork
+        return new Promise((resolve, reject) => {
+            const node = document.createElement("script");
+            node.onload = () => {
+                node.remove();
+                resolve();
+            };
+            node.src = "/.fn/" + fn_iri;
+            document.head.appendChild(node);
+        });
+    },
+    dep: (dependency) => {
+        return new Promise((resolve, reject) => {
+            const node = document.createElement("script");
+            node.onload = () => {
+                node.remove();
+                resolve();
+            };
+            node.src = "/.dp/" + dependency;
+            document.head.appendChild(node);
+        });
+    },
+    err: console.error;
+});
