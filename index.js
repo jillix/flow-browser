ba097603085ae9bf447025a4288e6363fc516ceb
Adrian Ottiker <adrian@ottiker.com>
2017-06-18 15:52:02 +0200

    put redunant code in a function

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const cache = {};
+function loadScript (name, src) {
+    return new Promise((resolve, reject) => {
+        const node = document.createElement("script");
+        node.onload = () => {
+            node.remove();
+            resolve(result);
+        };
+        node.src = src;
+        document.head.appendChild(node);
+    });
+}
 Flow({
     set: (key, val) => {
         return cache[key] = val;
@@ -12,35 +23,14 @@ Flow({
     seq: (sequenceId, role) => {
         // TODO here we could load a sequence from the safenetwork
         return fetch("/.sq/" + sequenceId).then((response) => {
-
-            if (!response.ok) {
-                return Promise.reject(response.statusText);
-            }
-
-            return response.json();
+            return response.ok ? response.json() : Promise.reject(response.statusText);
         });
     },
     fnc: (fn_iri, role) => {
-        // TODO here we could load the handler bundle form the safenetwork
-        return new Promise((resolve, reject) => {
-            const node = document.createElement("script");
-            node.onload = () => {
-                node.remove();
-                resolve(fn_iri);
-            };
-            node.src = "/.fn/" + fn_iri;
-            document.head.appendChild(node);
-        });
+        // TODO here we could load the handler script form the safenetwork
+        loadScript(fn_iri, "/.fn/" + fn_iri);
     },
     dep: (name, dependency) => {
-        return new Promise((resolve, reject) => {
-            const node = document.createElement("script");
-            node.onload = () => {
-                node.remove();
-                resolve(name);
-            };
-            node.src = "/.dp/" + dependency;
-            document.head.appendChild(node);
-        });
+        loadScript(name, "/.dp/" + dependency);
     }
 });
