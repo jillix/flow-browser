b8fbe52e37164501ecca11dbc02f651e02ad4378
Adrian Ottiker <adrian@ottiker.com>
2017-11-26 17:55:34 +0000

    implement safenet client

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
+import Safe from "./safe.js";
 import Flow from "./flow.js";
 
-export default (sequences, handlers, dependencies, initSequence) => {
-    const cache = {};
-    return Flow({
-        set: (key, val) => {
-            return cache[key] = val;
-        },
-        get: (key) => {
-            return cache[key];
-        },
-        del: (key) => {
-            delete cache[key];
-        },
-        seq: (sequenceId, role) => {
-            return fetch(sequences + "/" + sequenceId + ".json").then((response) => {
-                return response.ok ? response.json() : Promise.reject(response.statusText);
-            });
-        },
-        fnc: (fn_iri, role) => {
-            return fetch(handlers + "/" + fn_iri + ".js").then((response) => {
-                return response.text().then((script) => {
-                    return new Function("Adapter", "flow", script);
+export default (app_config, initSequence) => {
+
+    return Safe(app_config)
+    .then((app) => {
+        const cache = {};
+        return Flow({
+            set: (key, val) => {
+                return cache[key] = val;
+            },
+            get: (key) => {
+                return cache[key];
+            },
+            del: (key) => {
+                delete cache[key];
+            },
+            seq: (addr) => {
+                return app.idata.read(addr).then((data) => {
+                    console.log(data.toString())
+                    return JSON.parse(data.toString());
                 });
-            });
-        },
-        dep: (name, dependency) => {
-            return new Promise((resolve, reject) => {
-            const node = document.createElement("script");
-            node.onload = () => {
-                node.remove();
-                resolve(name);
-            };
-            node.src = dependencies + "/" + name + ".js";
-            node.type = "text/javascript";
-            document.head.appendChild(node);
-        });
-        }
-    })(initSequence);
+            },
+            fnc: (addr) => {
+                return app.idata.read(addr).then((data) => {
+                    console.log(data.toString());
+                    return new Function("Adapter", "flow", data.toString());
+                });
+            },
+            dep: (name, dependency) => {
+                return new Promise((resolve, reject) => {
+                    const node = document.createElement("script");
+                    node.onload = () => {
+                        node.remove();
+                        resolve(name);
+                    };
+                    node.src = dependencies + "/" + name + ".js";
+                    node.type = "text/javascript";
+                    document.head.appendChild(node);
+                });
+            }
+        })(initSequence);
+    });
 }
